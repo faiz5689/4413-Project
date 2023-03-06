e2d1cee17ea81b533861ba65f05a1f93bfe8fda3
Stefan Petrovic <petroste@my.yorku.ca>
2023-03-06 17:49:08 -0500

    added models for db

---
+++ backend/models/orders.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+    {
+      orderItems: [
+        {
+          name: { type: String, required: true },
+          quantity: { type: Number, required: true },
+          image: { type: String, required: true },
+          price: { type: Number, required: true },
+          product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+          },
+        },
+      ],
+      totalPrice: { type: Number, required: true },
+      customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
+    },
+    {
+      timestamps: true,
+    }
+  );
+  const Order = mongoose.model('Order', orderSchema);
+  export default Order;
