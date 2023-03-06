11b6e00510cf50edf2b7283f11c6f3f17daf6fe6
dnsantosuosso <dnsantosuosso@gmail.com>
2023-03-06 15:57:06 -0500

    go faiz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
       <header className="App-header">
         <a href="/">project 4413</a>
       </header>
-      <main>list products!! DIEGO testing push</main>
+      <main>list products!! FAIZ testing push</main>
     </div>
   );
 }
