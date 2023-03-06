362e9fd368367d0d3e5c9d210d369952171b7b35
Stefan Petrovic <petroste@Stefans-MacBook-Pro.local>
2023-03-06 15:47:12 -0500

    backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
+import React, { Component }  from 'react';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">project 4413</a>
       </header>
+<<<<<<< HEAD
       <main>list products!! Diego testing again</main>
+=======
+      <main>list products 2</main>
+>>>>>>> 6a4aa7e (backend)
     </div>
   );
 }
