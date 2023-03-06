4ab02304611ae6961c0bf6065bad384a23be3b0b
Stefan Petrovic <petroste@my.yorku.ca>
2023-03-06 16:41:24 -0500

    typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,7 @@ function App() {
       <header className="App-header">
         <a href="/">project 4413</a>
       </header>
-<<<<<<< HEAD
       <main>list products!! Diego testing again</main>
-=======
-      <main>list products 2</main>
->>>>>>> 6a4aa7e (backend)
     </div>
   );
 }
