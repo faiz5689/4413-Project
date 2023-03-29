d86b493cc27981bb8c1bdc1fef9e206ee0bcfb51
Faiz <fa.incisive@gmail.com>
2023-03-29 03:55:37 -0400

    some convention changes

--- frontend/src/Component/landing/landingHeader.js
+++ frontend/src/Component/landing/landingHeader.js
@@ -3,17 +3,17 @@ import { useEffect, useState } from 'react';
 import { makeStyles } from '@mui/styles';
 import { Collapse, IconButton } from '@mui/material';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import { Link as Scroll } from 'react-scroll'
+import { Link as Scroll } from 'react-scroll';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    justifyContent:"center",
-    alignItems:'center',
+    justifyContent: 'center',
+    alignItems: 'center',
     height: '100vh',
     fontFamily: 'Nunito',
   },
-  colorText:{
+  colorText: {
     color: '#5553b7',
   },
   container: {
@@ -40,26 +40,32 @@ export default function LandingHeader() {
   }, []);
 
   return (
-   
-    <div className={classes.root} id ="header">
+    <div className={classes.root} id="header">
       <Collapse
         in={checked}
         {...(checked ? { timeout: 1000 } : {})}
         collapsedHeight={50}
       >
-      <div className={classes.container}>
-        <h1 className={classes.title}>Welcome to <br/> <span className={classes.colorText}>SunSationalShades.</span></h1>
-        <h3 className={classes.title2}> Begin To See The World <span className={classes.colorText}>SunSationally.</span></h3>
-        <Scroll to="landing-content" smooth={true}>
-          <IconButton>
-            <KeyboardArrowDownIcon className={classes.goDown} sx={{fontSize: '4rem'}} />
-          </IconButton>
-        </Scroll>
-      </div>
+        <div className={classes.container}>
+          <h1 className={classes.title}>
+            Welcome to <br />{' '}
+            <span className={classes.colorText}>SunSationalShades.</span>
+          </h1>
+          <h3 className={classes.title2}>
+            {' '}
+            Begin To See The World{' '}
+            <span className={classes.colorText}>SunSationally.</span>
+          </h3>
+          <Scroll to="landing-content" smooth={true}>
+            <IconButton>
+              <KeyboardArrowDownIcon
+                className={classes.goDown}
+                sx={{ fontSize: '4rem' }}
+              />
+            </IconButton>
+          </Scroll>
+        </div>
       </Collapse>
     </div>
-    
-    
   );
-
 }
