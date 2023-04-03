ff88d4536a1886e7bcc84fb06550175d101d7117
Diego Santosuosso <diegosantosuosso@Diegos-MBP.ht.home>
2023-04-03 18:26:45 -0400

    Fixed yellow bars

--- frontend/src/Component/navbar/Header.js
+++ frontend/src/Component/navbar/Header.js
@@ -14,36 +14,47 @@ import { useLocation } from 'react-router-dom';
 const Header = () => {
   const [value, setValue] = useState();
   const navigate = useNavigate();
-  const pathName  = useLocation();
+  const pathName = useLocation();
   const dispatch = useDispatch();
-  var user = localStorage.getItem("username");
+  var user = localStorage.getItem('username');
 
   const handleLogout = (e) => {
-    if (user)
-    {
+    if (user) {
       e.preventDefault();
       dispatch(signout(user));
-      localStorage.removeItem("username");
+      localStorage.removeItem('username');
       navigate('/');
+    } else {
+      alert('Something went wrong.');
     }
-    else
-    {
-      alert ("Something went wrong.");
-    }
-
   };
+
+  const tabsList = [
+    <Tab key="products" component={Link} to="/products" label="Products" />,
+    <Tab key="contact" component={Link} to="/contact" label="Contact" />,
+  ];
+
+  if (user) {
+    tabsList.push(
+      <Tab key="checkout" component={Link} to="/checkout" label="Checkout" />
+    );
+  }
 
   return (
     <React.Fragment>
-      <AppBar elevation={pathName.pathname === '/' ? 0 : 3}
+      <AppBar
+        elevation={pathName.pathname === '/' ? 0 : 3}
         sx={{
-          background: pathName.pathname === '/' ? 'transparent' : 'linear-gradient(109.6deg, rgb(9, 9, 121) 11.2%, rgb(144, 6, 161) 53.7%, rgb(0, 212, 255) 100.2%)',
-          position: pathName.pathname === '/' ? 'none' : 'sticky' ,
+          background:
+            pathName.pathname === '/'
+              ? 'transparent'
+              : 'linear-gradient(109.6deg, rgb(9, 9, 121) 11.2%, rgb(144, 6, 161) 53.7%, rgb(0, 212, 255) 100.2%)',
+          position: pathName.pathname === '/' ? 'none' : 'sticky',
         }}
       >
         <Toolbar>
-          <Button  component={Link} to="/" onClick={() => setValue(false)}>
-              <Icon icon="mdi:glasses" width="50" height="50" color="white"/>
+          <Button component={Link} to="/" onClick={() => setValue(false)}>
+            <Icon icon="mdi:glasses" width="50" height="50" color="white" />
           </Button>
           <Tabs
             sx={{ marginLeft: 'auto' }}
@@ -52,57 +63,58 @@ const Header = () => {
             onChange={(e, value) => setValue(value)}
             TabIndicatorProps={{ style: { background: '#FADA5E' } }}
           >
-            <Tab component={Link} to="/products" label="Products"></Tab>
-            {user ?
-            (<><Tab component={Link} to="/contact" label="Contact"></Tab>
-             <Tab component={Link} to="/checkout" label="Checkout"></Tab></>) :
-            (<><Tab component={Link} to="/contact" label="Contact"></Tab></>)
-            }
-            
+            {React.Children.toArray(tabsList)}
           </Tabs>
-          {user ?
-            ( <IconButton 
-              onClick={() => setValue(false)} 
-              component={Link} to="/cart" 
-              sx={{color:"white", marginLeft:'20%' }}>
-              <ShoppingCartIcon/>
-            </IconButton>) :
-            (<></>)
-          }
+          {user ? (
+            <IconButton
+              onClick={() => setValue(false)}
+              component={Link}
+              to="/cart"
+              sx={{ color: 'white', marginLeft: '20%' }}
+            >
+              <ShoppingCartIcon />
+            </IconButton>
+          ) : (
+            <></>
+          )}
 
-          {user ?
-            (<>
-            Welcome, {user}
-            <Button
-              onClick={handleLogout}
-              component={Link}
-              variant="contained"
-              color="primary"
-              sx={{ marginLeft: '5%' }}
-            >
-              LOGOUT
-            </Button></>) :
-            (<><Button
-              onClick={() => setValue(false)}
-              component={Link}
-              to="/login"
-              variant="contained"
-              color="primary"
-              sx={{ marginLeft: '30%' }}
-            >
-              LOGIN
-            </Button>
-            <Button
-              onClick={() => setValue(false)}
-              component={Link}
-              to="/register"
-              variant="contained"
-              color="primary"
-              sx={{ marginLeft: '10px' }}
-            >
-              SIGN UP
-            </Button></>)
-          }
+          {user ? (
+            <>
+              Welcome, {user}
+              <Button
+                onClick={handleLogout}
+                component={Link}
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: '5%' }}
+              >
+                LOGOUT
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={() => setValue(false)}
+                component={Link}
+                to="/login"
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: '30%' }}
+              >
+                LOGIN
+              </Button>
+              <Button
+                onClick={() => setValue(false)}
+                component={Link}
+                to="/register"
+                variant="contained"
+                color="primary"
+                sx={{ marginLeft: '10px' }}
+              >
+                SIGN UP
+              </Button>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </React.Fragment>
--- frontend/src/pages/cart.js
+++ frontend/src/pages/cart.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import User from '../models/customer.model.js';
+
+// Assuming you have a variable called 'username' with the desired username
+const customer = await User.findOne({ username: username });
+const cart = customer.cart;
 
 const Cart = () => {
-  return <div>This is the Cart Page</div>;
+  const [cartItems, setCartItems] = useState(cart);
+
+  // Function to add an item to the cart
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  // Function to remove an item from the cart
+  const removeFromCart = (item) => {
+    const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+    setCartItems(updatedCart);
+  };
+
+  // Function to calculate the total cost of all items in the cart
+  const calculateTotal = () => {
+    return cartItems.reduce((total, item) => total + item.price, 0);
+  };
+
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.id}>
+              <h3>{item.name}</h3>
+              <p>{item.price}</p>
+              <button onClick={() => removeFromCart(item)}>
+                Remove from Cart
+              </button>
+            </div>
+          ))}
+          <h3>Total: {calculateTotal()}</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
