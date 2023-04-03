48c401d8d4877b92eee972cd10e87cf755fb27ec
Diego Santosuosso <diegosantosuosso@Diegos-MBP.ht.home>
2023-04-03 17:19:49 -0400

    Added video & details to landing page

--- frontend/src/pages/cart.js
+++ frontend/src/pages/cart.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Cart = () => {
-    return (
-        <div>
-            This is the Cart Page
-        </div>
-    );
+  return <div>This is the Cart Page</div>;
 };
 
 export default Cart;
