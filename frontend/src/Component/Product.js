e5dea44db35ecbf3312bc81952df78b3ad1c8915
Faiz <fa.incisive@gmail.com>
2023-04-04 01:47:04 -0400

    Some styling changes

--- frontend/src/Component/Product.js
+++ frontend/src/Component/Product.js
@@ -4,21 +4,20 @@ import { Link } from 'react-router-dom';
 export default function Product(props) {
   const { product } = props;
   return (
-    <div className='container'>
-        <div key={product._id} className="card">
-            <Link to={`/products/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-            </Link>
+    <div className="container">
+      <div key={product._id} className="card custom-card">
+        <Link to={`/products/${product._id}`}>
+          <img className="medium" src={product.image} alt={product.name} />
+        </Link>
         <div className="card-body">
-             <Link to={`/products/${product._id}`}>
-                <h2>{product.name}</h2>
-            </Link>
-            <div className="row">
-                <div className="price">${product.price}</div>
-            </div>
+          <Link to={`/products/${product._id}`}>
+            <h2>{product.name}</h2>
+          </Link>
+          <div className="row">
+            <div className="price">${product.price}</div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-
   );
 }
