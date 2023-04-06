3a19634887d3e709c39f8fba02cb36728edd5944
Diego Santosuosso <diegosantosuosso@Diegos-MacBook-Pro.local>
2023-04-06 15:17:10 -0400

    Adding reviews

--- frontend/src/pages/productDetails.js
+++ frontend/src/pages/productDetails.js
@@ -17,6 +17,7 @@ export default function ProductDetails(props) {
   const { error, product } = productDetails;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
+  var user = localStorage.getItem('username');
 
   // Log the entire Redux state
   const state = useSelector((state) => state);
@@ -93,6 +94,84 @@ export default function ProductDetails(props) {
           </div>
         </div>
       )}
+      <div style={{ margin: 100 }}>
+        <h1>Reviews:</h1>
+        <ul style={{ listStyleType: 'none', padding: 0 }}>
+          {product.reviews?.map((review, index) => (
+            <li
+              key={index}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                margin: '20px 0',
+                padding: '20px',
+                backgroundColor: '#f7f7f7',
+                borderRadius: '8px',
+                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+              }}
+            >
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div>
+                  <h2
+                    style={{
+                      fontSize: '18px',
+                      fontWeight: 'bold',
+                      marginBottom: 0,
+                    }}
+                  >
+                    Name: {review.name}
+                  </h2>
+                </div>
+              </div>
+              <p
+                style={{
+                  fontSize: '16px',
+                  lineHeight: '1.5',
+                  margin: '7px 0',
+                }}
+              >
+                Comment: {review.comment}
+              </p>
+              <div
+                style={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  width: '100%',
+                }}
+              >
+                <div
+                  style={{
+                    width: '100px',
+                    height: '16px',
+                    position: 'relative',
+                    borderRadius: '8px',
+                    backgroundColor: '#f1f1f1',
+                    overflow: 'hidden',
+                    marginRight: '10px',
+                  }}
+                >
+                  <div
+                    style={{
+                      width: `${(review.rating / 5) * 100}%`,
+                      height: '100%',
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                      backgroundColor: '#ffa500',
+                    }}
+                  ></div>
+                </div>
+                <p style={{ fontSize: '14px', color: '#888' }}>
+                  {review.rating}/5
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
