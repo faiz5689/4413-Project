118caedea26548d269a770724f245bdce8e639e4
Stefan Petrovic <petroste@my.yorku.ca>
2023-04-07 19:49:56 -0400

    fix Yazan's errors

--- frontend/src/pages/productDetails.js
+++ frontend/src/pages/productDetails.js
@@ -3,29 +3,61 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { detailsProduct } from '../actions/product.actions.js';
 import axios from 'axios';
-
 const API_URL = 'http://localhost:4000/api/users';
-
+const inventory = 'http://localhost:4000/api/review';
 export default function ProductDetails(props) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const params = useParams();
   const { id: productId } = params;
-
+  const [rating, setRating] = useState(0);
+  const [reload, setReload] = useState();
   const productDetails = useSelector((state) => state.productDetails);
   const { error, product } = productDetails;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   var user = localStorage.getItem('username');
 
+  //if user is signed in, then they can make a review 
+  const addreview = async (e) => {
+    if (user) {
+      e.preventDefault();
+      console.log (comment);
+      console.log (rating);
+      console.log (user);
+      try {
+        await axios.post(`${inventory}/add`, {
+        inventoryName: product.name, username: user, comment: comment, rating: rating,
+        }, { withCredentials: true }).then(response => {
+          alert('Your comment and rating has been added!');
+          setReload(!reload);
+          setComment('');
+          setRating(0);
+          // Do something with the response, such as update the UI or show a success message
+        });
+        
+      } catch (error) {
+        console.error('Error adding review', error);
+      }
+  
+    } else {
+      alert('User is not logged in!');
+    }
+  };
+  //this function is for grabing the text from the text area when the user make a comment 
+  const [comment, setComment] = useState('');
+
+  function handleCommentChange(event) {
+    setComment(event.target.value);
+  }
   // Log the entire Redux state
   const state = useSelector((state) => state);
   console.log('Redux state:', state);
 
   useEffect(() => {
     dispatch(detailsProduct(productId));
-  }, [dispatch, productId]);
+  }, [dispatch, productId, reload]);
 
   const addToCartHandler = async () => {
     //userInfo: id, username, isAdmin, loyaltypoints
@@ -36,8 +68,8 @@ export default function ProductDetails(props) {
           `${API_URL}/add-to-cart/${user._id}`,
           {
             id: productId,
-          },
-          { withCredentials: true }
+          }
+          , { withCredentials: true }
         );
         alert('Item added to cart!');
       } catch (error) {
@@ -58,7 +90,7 @@ export default function ProductDetails(props) {
       ) : (
         <div className="container">
           <div class="left-side">
-            <img
+          <img
               className="large"
               src={product.image}
               alt={product.name}
@@ -105,8 +137,57 @@ export default function ProductDetails(props) {
         </div>
       )}
       <div style={{ margin: 100 }}>
-        <h1>Reviews:</h1>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
+        <h1>
+          Reviews:
+        </h1>
+        <div>
+        {user ? 
+        (        <form style={{ maxWidth: '600px', margin: '0 auto' }}>
+        <label style={{ fontSize: '20px', fontWeight: 'bold', marginBottom: '15px', display: 'block' }}>
+          Write your review:
+        </label>
+        <textarea
+         value={comment}
+         onChange={handleCommentChange}
+          name="review"
+          style={{
+            width: '100%',
+            height: '120px',
+            padding: '10px',
+            marginBottom: '20px',
+            borderRadius: '4px',
+            border: '1px solid #a9a9a9',
+            fontSize: '16px',
+            lineHeight: '1.5',
+          }}
+        ></textarea>
+        
+        <label style={{ fontSize: '20px', fontWeight: 'bold', marginBottom: '15px', display: 'block' }}>
+          Rate the product:
+        </label>
+        <div style={{ marginBottom: '12px' }}>
+          <span onClick={() => setRating(1)}  style={{fontSize: '42px'}}>
+            {rating >= 1 ? '★' : '☆'}
+          </span>
+          <span onClick={() => setRating(2)}  style={{fontSize: '42px'}}>
+            {rating >= 2 ? '★' : '☆'}
+          </span>
+          <span onClick={() => setRating(3)}  style={{fontSize: '42px'}}>
+            {rating >= 3 ? '★' : '☆'}
+          </span>
+          <span onClick={() => setRating(4)}  style={{fontSize: '42px'}}>
+            {rating >= 4 ? '★' : '☆'}
+          </span>
+          <span onClick={() => setRating(5)}  style={{fontSize: '42px'}}>
+            {rating >= 5 ? '★' : '☆'}
+          </span>
+        </div>
+        <button onClick={addreview} type="submit" style={{ backgroundColor: '#f0c14b', color: '#111', padding: '10px 20px', border: 'none', borderRadius: '4px', fontSize: '18px', cursor: 'pointer', marginTop: '20px' }}>Submit</button>
+      </form>) :
+        (<></>)
+        }
+
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
           {product.reviews &&
             product.reviews.map((review, index) => (
               <li
@@ -181,7 +262,8 @@ export default function ProductDetails(props) {
                 </div>
               </li>
             ))}
-        </ul>
+          </ul>
+        </div>
       </div>
     </div>
   );
