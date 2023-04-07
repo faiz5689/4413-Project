231b09b52b0e7a4091e9a51c95e820798140a355
Faiz <fa.incisive@gmail.com>
2023-04-07 09:29:24 -0400

    Added Virtual Try On

--- frontend/src/Component/Product.js
+++ frontend/src/Component/Product.js
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { render } from 'react-dom';
 import ReactAppContainer from 'react-app-container';
-// import { GlassArView } from '@lastcode802/glassarview';
+import { GlassArView } from '@lastcode802/glassarview';
 
 export default function Product(props) {
   const { product } = props;
-  // const jeelizvto = () => {
-  //   render(
-  //     <ReactAppContainer>
-  //       <GlassArView
-  //         modelname={`${product.sku}`}
-  //         canvasheight={500}
-  //         canvaswidth={500}
-  //       />
-  //     </ReactAppContainer>,
-  //     document.querySelector('#root')
-  //   );
-  // };
+  const jeelizvto = () => {
+    render(
+      <ReactAppContainer>
+        <GlassArView
+          modelname={`${product.sku}`}
+          canvasheight={500}
+          canvaswidth={500}
+        />
+      </ReactAppContainer>,
+      document.querySelector('#root')
+    );
+  };
 
   return (
     <div className="container">
@@ -31,7 +31,7 @@ export default function Product(props) {
           </Link>
           <div className="row">
             <div className="price">${product.price}</div>
-            {/* <button onClick={jeelizvto}>Try-On</button> */}
+            <button onClick={jeelizvto}>Try-On</button>
           </div>
         </div>
       </div>
