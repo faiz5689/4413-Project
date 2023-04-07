f8d977fecbf350e85c96b96d2d3414ec037001c4
Faiz <fa.incisive@gmail.com>
2023-04-07 06:23:14 -0400

    virtual try on

--- frontend/src/Component/Product.js
+++ frontend/src/Component/Product.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { render } from 'react-dom';
+// import { AppContainer } from 'react-hot-loader';
+import ReactAppContainer from 'react-app-container';
+import { GlassArView } from '@lastcode802/glassarview';
+
+// import '../styles/jeeliz.css';
+
+import AppCanvas from './AppCanvas';
 
 export default function Product(props) {
   const { product } = props;
+  const [showCanvas, setShowCanvas] = useState(false);
+  const jeelizvto = () => {
+    console.log('jello');
+    render(
+      <ReactAppContainer>
+        <GlassArView
+          modelname={`${product.sku}`}
+          canvasheight={500}
+          canvaswidth={500}
+        />
+      </ReactAppContainer>,
+      document.querySelector('#root')
+    );
+  };
+
   return (
     <div className="container">
       <div key={product._id} className="card custom-card">
@@ -15,6 +38,8 @@ export default function Product(props) {
           </Link>
           <div className="row">
             <div className="price">${product.price}</div>
+            <button onClick={jeelizvto}>Try-On</button>
+            {showCanvas && <AppCanvas />}
           </div>
         </div>
       </div>
