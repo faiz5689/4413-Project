4b5c3da17c750650949c1d7fa46e9af814e9386b
arezaevv <a@rezaev.com>
2023-04-08 18:41:34 -0400

    More frontend changes

--- frontend/src/Component/Product.js
+++ frontend/src/Component/Product.js
@@ -28,12 +28,39 @@ export default function Product(props) {
           <img className="medium" src={product.image} alt={product.name} />
         </Link>
         <div className="card-body">
-          <Link to={`/products/${product._id}`}>
-            <h2>{product.name}</h2>
+          <Link
+            to={`/products/${product._id}`}
+            style={{ textDecoration: 'none' }}
+          >
+            <h2
+              style={{
+                fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                color: '#333',
+                fontWeight: 'bold',
+              }}
+            >
+              {product.name}
+            </h2>
           </Link>
           <div className="row">
             <div className="price">${product.price}</div>
-            <button onClick={jeelizvto}>Try-On</button>
+            <button
+              onClick={jeelizvto}
+              style={{
+                fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+                backgroundColor: '#7865f5',
+                color: '#fff',
+                padding: '10px 20px',
+                border: 'none',
+                borderRadius: '5px',
+                cursor: 'pointer',
+                transition: 'background-color 0.3s ease-in-out',
+              }}
+              onMouseOver={(e) => (e.target.style.backgroundColor = '#4E2A84')}
+              onMouseOut={(e) => (e.target.style.backgroundColor = '#7865f5')}
+            >
+              Try-On
+            </button>
           </div>
         </div>
       </div>
